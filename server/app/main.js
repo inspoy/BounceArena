67e5a287df74a1a52c614c6b2e8de9ed33a88dfe
Inspoy <inspoy@163.com>
2017-03-26 21:57:18 +0800

    - 最简单的TCP服务器

--- server/app/main.js
+++ server/app/main.js
@@ -1,1 +1,8 @@
-
+const net = require("net");
+net.createServer(function(socket) {
+	socket.on("data", function(data) {
+		console.log("收到了：" + data);
+		socket.write("收到了：" + data);
+	});
+	socket.write("Hello!");
+}).listen(12345);
